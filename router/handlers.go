f2a049de22b20d66c68bdc3bee34c5fcea3eb534
James <j.wallen.jones@googlemail.com>
2015-12-24 10:09:57 +0000

    return http headers

--- router/handlers.go
+++ router/handlers.go
@@ -20,7 +20,8 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		todo.Todo{Name: "Learn Golang"},
 		todo.Todo{Name: "Write some awesome code"},
 	}
-
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
