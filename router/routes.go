ffc7b5dcf7ba5835a8ed67a401bc2fa386e36090
James <j.wallen.jones@googlemail.com>
2015-12-24 00:25:00 +0000

    add a http logger

--- router/routes.go
+++ router/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jaaaaaaaaaam/go-todo/logger"
 )
 
 // Route is a struct
@@ -22,11 +23,16 @@ func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = logger.Logger(handler, route.Name)
+
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
-			Handler(route.HandlerFunc)
+			Handler(handler)
 	}
 
 	return router
